a139be98a6beee36d8517dfe641a872108f11ae2
pachong <lfx285@163.com>
2021-07-06 14:48:09 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,5 +7,7 @@
 #=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+#swich theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci/Makefile
 #2. Replace with JerryKuKu’s Argon
 rm openwrt/package/lean/luci-theme-argon -rf
